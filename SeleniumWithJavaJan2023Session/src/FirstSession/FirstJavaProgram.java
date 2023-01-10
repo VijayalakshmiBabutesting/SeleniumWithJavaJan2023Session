d6eb2adff6d00c771167803569d8b9074ad22fe5
mchugh <manishchugh1981@gmail.com>
2023-01-10 12:47:40 -0500

    Jan 9

---
+++ SeleniumWithJavaJan2023Session/src/FirstSession/FirstJavaProgram.java
@@ -0,0 +1,22 @@
+//Jan 9-2023-P1
+package FirstSession;
+/**
+ * 
+ * @author mchugh
+ * This program is about Java basics
+ */
+public class FirstJavaProgram {
+
+	
+	public static void main(String[] args) {
+
+		System.out.println("My first Java Program and I am happy");
+		//syso+cntr+spce -> single line comment
+		/*
+		 * multi line comments
+		 */
+		System.out.println("Hi");
+		
+	}
+
+}
