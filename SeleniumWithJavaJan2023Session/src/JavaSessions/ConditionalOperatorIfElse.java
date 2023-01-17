56dabb0eeef3d9699af47a22331b37125fbf09a6
mchugh <manishchugh1981@gmail.com>
2023-01-16 22:31:56 -0500

    Jan 16

--- SeleniumWithJavaJan2023Session/src/JavaSessions/ConditionalOperatorIfElse.java
+++ SeleniumWithJavaJan2023Session/src/JavaSessions/ConditionalOperatorIfElse.java
@@ -68,8 +68,94 @@ public class ConditionalOperatorIfElse {
 		  }
 		  else
 			  System.out.println("FAIL");
-		  
-		  
+		 
+		System.out.println("------- ");  
+		int balance=100;
+		if(balance!=100) {
+			System.out.println("balance is not correct");
+		}
+		System.out.println("------- "); 
+		//WAP to find the greatest of 3 numbers.
+		
+		int x=1000;
+		int y=200;
+		int z=600;
+	
+		if(x>y && x>z) {
+			System.out.println("x is the greatest");
+		} else if(y>z) {
+			System.out.println("y is the greatest");
+		} else {
+			System.out.println("z is the greatest");
+		}
+		System.out.println("------- XXX------- "); 
+		//WAP to check browser value and then launch the respective browser.
+		
+		//1. if
+		String browsers="opera";
+		
+//		if(browsers.equals("chrome")) {
+//			System.out.println("Launch chrome browser");
+//		}
+//			
+//		if(browsers.equals("firefox")) {
+//			System.out.println("Launch firefox browser");
+//		}
+//		
+//		if(browsers.equals("ie")) {
+//			System.out.println("Launch ie browser");
+//		}
+//			
+//		if(browsers.equals("safari")) {
+//			System.out.println("Launch safari browser");
+//		}	
+//			
+//		else {
+//			System.out.println("please pass the correct browser");
+//		}
+		//comment - command + / to comment and uncomment selected lines
+		//2. if -elseif-else	
+			
+		if(browsers.equals("chrome")) {
+			System.out.println("Launch chrome browser");
+		}
+		
+		else if(browsers.equals("firefox")) {
+			System.out.println("Launch firefox browser");
+		}
+		
+		else if(browsers.equals("ie")) {
+			System.out.println("Launch ie browser");
+		}
+		
+		else if(browsers.equals("safari")) {
+			System.out.println("Launch safari browser");
+		}
+		
+		else {
+			System.out.println("please pass the correct browser");
+		}
+		
+		//3. switch-case --> control+space
+//		switch (browsers) {
+//		case "chrome":
+//			System.out.println("Launch chrome browser");
+//			break;
+//		case "firefox":
+//			System.out.println("Launch firefox browser");
+//			break;
+//		case "ie":
+//			System.out.println("Launch ie browser");
+//			break;
+//		case "safari":
+//			System.out.println("Launch safari browser");
+//			break;
+//		default:
+//			System.out.println("please pass the correct browser");
+//			break;
+//		}
+		
+	
 	}
 
 }
---
+++ SeleniumWithJavaJan2023Session/src/JavaSessions/LoopsConcept.java
@@ -0,0 +1,22 @@
+//Jan 16-2023-p1
+package JavaSessions;
+
+public class LoopsConcept {
+
+	public static void main(String[] args) {
+
+		// print 1 to 10
+		// loops
+		// 1. While
+		
+		int i=1;
+		while(i<11) {
+			System.out.println(i);
+			i++;//increments the value of i by 1
+		}
+		
+	
+		
+	}
+
+}
