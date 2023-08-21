002d42a2e89e914fce9816dedb9533caa06ae080
mchugh <manishchugh1981@gmail.com>
2023-08-20 22:32:44 -0400

    August 16

---
+++ SeleniumWithMavenJan2023Session/src/main/java/SeleniumSessions/CalendarHandle.java
@@ -0,0 +1,53 @@
+//Aug 17 2023,p1
+package SeleniumSessions;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CalendarHandle {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.ixigo.com/");
+		Thread.sleep(5000);
+
+		driver.findElement(By.xpath("//input[@placeholder='Depart' and @type='text']")).click();
+
+		// start date
+		List<WebElement> startDateList = driver
+				.findElements(By.xpath("(//div[@class='rd-month'])[1]/table//div[contains(@class, 'day')]"));
+
+		for (WebElement e : startDateList) {
+			if (e.getText().equals("30")) {
+				e.click();
+				break;
+			}
+
+		}
+
+		driver.findElement(By.xpath("//input[@placeholder='Return' and @type='text']")).click();
+
+		Thread.sleep(2000);
+		// end date
+		List<WebElement> endDateList = driver
+				.findElements(By.xpath("(//div[@class='rd-month'])[4]/table//div[contains(@class, 'day')]"));
+
+		for (WebElement e : endDateList) {
+			if (e.getText().equals("15")) {
+				e.click();
+				break;
+			}
+
+		}
+
+	}
+
+}
